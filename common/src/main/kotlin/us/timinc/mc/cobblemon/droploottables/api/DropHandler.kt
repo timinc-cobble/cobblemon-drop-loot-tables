39d4f93e7a90d3055b386099b455b222aa769943
Timothy Metcalfe <tmetcalfe89@gmail.com>
2026-04-23 22:20:46 -0700

    minor typo fix

--- common/src/main/kotlin/us/timinc/mc/cobblemon/droploottables/api/DropHandler.kt
+++ common/src/main/kotlin/us/timinc/mc/cobblemon/droploottables/api/DropHandler.kt
@@ -93,7 +93,7 @@ interface DropHandler<C : DropContext, D : Dropper<C>, E> {
 
     fun processLegacyDrops(evt: E): List<ItemStack> = emptyList()
 
-    @Deprecated("Old pre-determined paths for loot tables, please use dopper data layer")
+    @Deprecated("Old pre-determined paths for loot tables, please use dropper data layer")
     fun getLegacyDrops(form: FormData,
                        dropType: String,
                        params: net.minecraft.world.level.storage.loot.LootParams,
@@ -101,11 +101,11 @@ interface DropHandler<C : DropContext, D : Dropper<C>, E> {
     ) = dropFromTable(getAllDropId(dropType), params, level) +
         dropFromTable(getFormDropId(form, dropType), params, level)
 
-    @Deprecated("Old pre-determined paths for loot tables, please use dopper data layer")
+    @Deprecated("Old pre-determined paths for loot tables, please use dropper data layer")
     private fun getAllDropId(dropType: String): ResourceLocation =
         ResourceLocation.fromNamespaceAndPath(MOD_ID, "$dropType/all")
 
-    @Deprecated("Old pre-determined paths for loot tables, please use dopper data layer")
+    @Deprecated("Old pre-determined paths for loot tables, please use dropper data layer")
     private fun getFormDropId(form: FormData, dropType: String): ResourceLocation =
         ResourceLocation.fromNamespaceAndPath(
             MOD_ID,
